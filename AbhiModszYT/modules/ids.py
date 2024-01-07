bf7e981fdae5fd610d37b6ad7e0784859d2ea3d7
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2024-01-07 12:06:24 +0530

    Update ids.py

--- AbhiModszYT/modules/ids.py
+++ AbhiModszYT/modules/ids.py
@@ -11,8 +11,8 @@ async def getid(client, message):
     message_id = message.id
     reply = message.reply_to_message
 
-    text = f"**[𝙈𝙚𝙨𝙨𝙖𝙜𝙚 𝙄𝘿 :]({message.link})** `{message_id}`\n"
-    text += f"**[𝙔𝙤𝙪𝙧 𝙄𝘿 :](tg://user?id={your_id})** `{your_id}`\n"
+    text = f"**[๏ ᴍᴇssᴀɢᴇ ɪᴅ ➛]({message.link})** `{message_id}`\n"
+    text += f"**[๏ ʏᴏᴜʀ ɪᴅ ➛](tg://user?id={your_id})** `{your_id}`\n"
 
     if not message.command:
         message.command = message.text.split()
@@ -24,27 +24,27 @@ async def getid(client, message):
         try:
             split = message.text.split(None, 1)[1].strip()
             user_id = (await client.get_users(split)).id
-            text += f"**[𝙐𝙨𝙚𝙧 𝙄𝘿 :](tg://user?id={user_id})** `{user_id}`\n"
+            text += f"**[๏ ᴜsᴇʀ ɪᴅ ➛](tg://user?id={user_id})** `{user_id}`\n"
 
         except Exception:
-            return await message.reply_text("𝙏𝙝𝙞𝙨 𝙐𝙨𝙚𝙧 𝘿𝙤𝙚𝙨𝙣'𝙩 𝙀𝙭𝙞𝙨𝙩.", quote=True)
+            return await message.reply_text("๏ ᴛʜɪs ᴜsᴇʀ ᴅᴏᴇsɴ'ᴛ ᴇxɪsᴛ.", quote=True)
 
-    text += f"**[𝙂𝙧𝙤𝙪𝙥/𝘾𝙝𝙖𝙩 𝙄𝙙 :](https://t.me/{chat.username})** `{chat.id}`\n\n"
+    text += f"**[๏ ɢʀᴏᴜᴘ/ᴄʜᴀᴛ ɪᴅ ➛](https://t.me/{chat.username})** `{chat.id}`\n\n"
 
     if (
         not getattr(reply, "empty", True)
         and not message.forward_from_chat
         and not reply.sender_chat
     ):
-        text += f"**[𝙍𝙚𝙥𝙡𝙮 𝙈𝙚𝙨𝙨𝙖𝙜𝙚 𝙄𝘿 :]({reply.link})** `{reply.id}`\n"
-        text += f"**[𝙍𝙚𝙥𝙡𝙮 𝙐𝙨𝙚𝙧 𝙄𝘿 :](tg://user?id={reply.from_user.id})** `{reply.from_user.id}`\n\n"
+        text += f"**[๏ ʀᴇᴘʟʏ ᴍᴇssᴀɢᴇ ɪᴅ ➛]({reply.link})** `{reply.id}`\n"
+        text += f"**[๏ ʀᴇᴘʟʏ ᴜsᴇʀ ɪᴅ ➛](tg://user?id={reply.from_user.id})** `{reply.from_user.id}`\n\n"
 
     if reply and reply.forward_from_chat:
-        text += f"ᴛʜᴇ ғᴏʀᴡᴀʀᴅᴇᴅ ᴄʜᴀɴɴᴇʟ, {reply.forward_from_chat.title}, ʜᴀs ᴀɴ ɪᴅ ᴏғ `{reply.forward_from_chat.id}`\n\n"
+        text += f"๏ ᴛʜᴇ ғᴏʀᴡᴀʀᴅᴇᴅ ᴄʜᴀɴɴᴇʟ, {reply.forward_from_chat.title}, ʜᴀs ᴀɴ ɪᴅ ᴏғ `{reply.forward_from_chat.id}`\n\n"
         print(reply.forward_from_chat)
 
     if reply and reply.sender_chat:
-        text += f"ɪᴅ ᴏғ ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ᴄʜᴀᴛ/ᴄʜᴀɴɴᴇʟ, ɪs `{reply.sender_chat.id}`"
+        text += f"๏ ɪᴅ ᴏғ ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ᴄʜᴀᴛ/ᴄʜᴀɴɴᴇʟ, ɪs `{reply.sender_chat.id}`"
         print(reply.sender_chat)
 
     await message.reply_text(
