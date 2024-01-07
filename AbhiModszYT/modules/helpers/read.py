8ffc55dde8bd3dcb88e3013355d695b9344c8f19
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2024-01-07 12:31:55 +0530

    Update read.py

--- AbhiModszYT/modules/helpers/read.py
+++ AbhiModszYT/modules/helpers/read.py
@@ -11,12 +11,7 @@ TOOLS_DATA_READ = f"""
 ๏ ʜᴇʀᴇ ɪs ᴛᴏᴏʟs ғᴏʀ {BOT_NAME} ! \n\n๏ ᴜsᴇ /repo ɢᴇᴛᴛɪɴɢ ᴍʏ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ. \n\n๏ ᴜsᴇ /ping ᴄʜᴇᴄᴋɪɴɢ ғᴏʀ ᴘɪɴɢ.\n\n๏ ᴜsᴇ /id ɢᴇᴛᴛɪɴɢ ʏᴏᴜʀ ᴄʜᴀᴛ ɪᴅ ᴀɴᴅ ᴜsᴇʀ ɪᴅ ᴏʀ ᴍᴇssᴀɢᴇ ɪᴅ."""
 
 CHATBOT_READ = f"""
-<u>**𝘾𝙈𝘿𝙎 𝙁𝙤𝙧 {BOT_NAME}**</u>
-** 𝙐𝙨𝙚 /chatbot 𝙏𝙤 𝙀𝙣𝙖𝙗𝙡𝙚/𝘿𝙞𝙨𝙖𝙗𝙡𝙚 𝘾𝙝𝙖𝙩𝘼𝙞.**
-**───────────────**
-**───────────────**
-<b>||©️ @{OWNER_USERNAME}||</b>
-"""
+๏ ᴄᴏᴍᴍᴀɴᴅ ғᴏʀ {BOT_NAME} !\n\n๏ ᴜsᴇ /chatbot ᴇɴᴀʙʟᴇ/ᴅɪsᴀʙʟᴇ .\n\n๏ ᴍᴀᴅᴇ ʙʏ ➛ [ʀᴏʏ-ᴇᴅɪᴛx](https://t.me/roy_editx)"""
 
 SOURCE_READ = f"**𝙃𝙚𝙡𝙡𝙤 𝘿𝙚𝙖𝙧\n 𝙎𝙤𝙪𝙧𝙘𝙚 𝘾𝙤𝙙𝙚 𝙊𝙁 [{BOT_NAME}](https://t.me/{BOT_USERNAME}).**\n**𝙋𝙡𝙨 𝙁𝙤𝙧𝙠 𝙏𝙝𝙚 𝙍𝙚𝙥𝙤 & 𝙂𝙞𝙫𝙚 𝙎𝙩𝙖𝙧 ✯**\n**──────────────────**\n** 𝙃𝙚𝙧𝙚 𝙄𝙨 [𝙎𝙤𝙪𝙧𝙘𝙚 𝘾𝙤𝙙𝙚](https://github.com/AbhiModszYT/chat-bot)**\n**──────────────────**\n**𝙄𝙛 𝙔𝙤𝙪 𝙁𝙖𝙘𝙚𝙞𝙣𝙜 𝘼𝙣𝙮 𝙋𝙧𝙤𝙗𝙡𝙚𝙢 𝘾𝙤𝙣𝙩𝙖𝙘𝙩 𝙈𝙚 [𝗔𝗠𝗕𝗢𝗧](https://t.me/AM_YTBOTT).\n\n\n<b>||𝘽𝙤𝙩 𝙊𝙬𝙣𝙚𝙧 : @{OWNER_USERNAME}||</b>"
 
